a7497577cba79b1e77fbdfbe347d3644bb5523c1
github <customme@163.com>
2018-12-20 14:20:30 +0800

    导入官方3.8版本

--- jeecg-framework/src/main/webapp/webpage/com/jeecg/pas/goodsClass.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-	// 表格样式
-	$("#formobj table").attr("cellpadding", "0");
-	$("#formobj table").attr("cellspacing", "1");
-});
--- jeecg-framework/src/main/webapp/webpage/com/jeecg/pas/shop.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	// 设置个性化样式
-	$("#shopName").css("width", "200px");
-	$("#tradeArea").css("width", "200px");
-	$("#address").css("width", "300px");
-});
